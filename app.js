6d582470c0bd498bb612be06b9240f042ff3ee2a
denisk13005 <kirmanndenis@gmail.com>
2022-08-15 12:03:50 +0200

    niveau 4 refacto router controllers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Thing = require('./models/Thing');
+const Product = require('./models/Product') //TP fin partie 3
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI || `mongodb+srv://denisk13005:Lucas*2808@cluster0.upft3lg.mongodb.net/?retryWrites=true&w=majority`,
@@ -18,7 +19,7 @@ app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
    next(); // on n'oublie pas le next 
  });
- //route post
+//  route post
  app.post('/api/stuff', (req, res, next) => {
   delete req.body._id;
   const thing = new Thing({ // on crée une instance de Thing avec le corp de la requête
@@ -53,4 +54,34 @@ app.get('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+// *****************************************************************************TP fin partie 2
+// app.post('/api/products',(req,res,next)=> {
+//   const product = new Product({
+//     ...req.body
+//   })
+//   console.log(product)
+//        product.save() // on l'ajoute a la bdd grâce à la méthode .save
+//          .then(() => res.status(201).json({product}))
+//         .catch(error => res.status(400).json({ error }));
+// })
+// app.put('/api/products/:id', (req, res, next) => {
+//    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id }) // on remet l'id car on ne peut pas modifier un paramètre immutable comme l'id 
+//      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+//      .catch(error => res.status(400).json({ error }));
+//  });
+// app.delete('/api/products/:id', (req, res, next) => {
+//     Product.deleteOne({ _id: req.params.id })
+//      .then(() => res.status(200).json({ message: 'Deleted'}))
+//      .catch(error => res.status(400).json({ error }));
+//    });
+// app.get('/api/products/:id',(req,res,next)=> {
+//   Product.findOne({_id: req.params.id})
+//     .then(product=> res.status(200).json({ product}))
+//     .catch(error => res.status(400).json(error))
+// })
+// app.get('/api/products',(req,res,next)=> {
+//   Product.find()
+//     .then(products => res.status(200).json({products}))
+//     .catch(error => res.status(400).json(error))
+// })
 module.exports = app;
---
+++ models/Product.js
@@ -0,0 +1,12 @@
+// créer pour le TP partie 3
+
+// const mongoose = require('mongoose');
+
+// const productSchema = mongoose.Schema({
+//   name: { type: String, required: true },
+//   description: { type: String, required: true },
+//   inStock: { type: Boolean, required: true },
+//   price: { type: Number, required: true },
+// });
+
+// module.exports = mongoose.model('Product', productSchema);
