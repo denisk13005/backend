4c08ccf9bbc8af0dd075f68c255ec0c7587cc715
denisk13005 <kirmanndenis@gmail.com>
2022-08-15 14:48:10 +0200

    création du des controllers

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,41 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-const Thing = require('../models/Thing')
+const stuffCtrl = require('../controllers/stuff');// on appelle le controller qui va gérer la logique métier grâce à des fontions que l'on passera en callback dans la méthode voulue de notre router 
 
-//  route post
-router.post('/', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({ // on crée une instance de Thing avec le corp de la requête
-    ...req.body
-  });
-  thing.save() // on l'ajoute a la bdd grâce à la méthode .save
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+router.get('/', stuffCtrl.getAllStuff);
+router.post('/', stuffCtrl.createThing);
+router.get('/:id', stuffCtrl.getOneThing);
+router.put('/:id', stuffCtrl.modifyThing);
+router.delete('/:id', stuffCtrl.deleteThing);
 
-// PUT
-router.put('/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id }) // on remet l'id car on ne peut pas modifier un paramètre immutable comme l'id 
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-//DELETE
-router.delete('/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-//get un objet grace à son id passé en paramètre dans l'url
-router.get('/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id }) //on extrait l'objet grâce à findOne
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-router.get('/', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
-
-module.exports = router
+module.exports = router;
