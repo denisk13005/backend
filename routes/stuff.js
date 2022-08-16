45e399744f54144d2942c7636babebe35b7d6378
denisk13005 <kirmanndenis@gmail.com>
2022-08-16 15:22:48 +0200

    essai de suppression de l'auth sur la route get de stuff

--- routes/stuff.js
+++ routes/stuff.js
@@ -4,7 +4,7 @@ const stuffCtrl = require('../controllers/stuff');
 const multer = require('../middlewares/multer-config')
 const router = express.Router();
 
-router.get('/', auth, stuffCtrl.getAllStuff);
+router.get('/',  stuffCtrl.getAllStuff);
 router.post('/', auth,multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
 router.put('/:id', auth, multer,stuffCtrl.modifyThing);
