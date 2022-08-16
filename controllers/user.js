46b2c7fbd73732665ec00aea91b188d1ec169115
denisk13005 <kirmanndenis@gmail.com>
2022-08-16 09:16:53 +0200

    installation et envoie du token d'authentification avec jwt

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
@@ -36,7 +37,11 @@ exports.login = (req, res, next) => {
                   }
                   res.status(200).json({
                       userId: user._id,
-                      token: 'TOKEN'
+                      token: jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' }
+                    )
                   });
               })
               .catch(error => res.status(500).json({ error }));
